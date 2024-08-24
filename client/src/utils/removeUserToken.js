453c64d3bbaf57f04c4a79d7ec34effaa65b21e1
chittelk <158280687+chittelk@users.noreply.github.com>
2024-08-24 19:21:37 +0100

    client-package.json
    package.json

--- client/src/utils/removeUserToken.js
+++
@@ -1,9 +0,0 @@
-import Cookies from "universal-cookie";
-
-const cookies = new Cookies();
-
-export const removeUserToken = () => {
-    cookies.remove("user_token", {
-        path: "/",
-    });
-};
